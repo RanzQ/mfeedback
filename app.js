3d2b1bd491191a94e59142098d8d2d76c1845072
Juha Rantanen <juha.rantanen@aalto.fi>
2012-02-23 20:50:28 +0200

    Initial server with express, jade, stylus, nib and jQuery Mobile.

---
+++ app.js
@@ -0,0 +1,62 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , app = module.exports = express.createServer()
+  , routes = require('./routes')
+  , viewEngine = 'jade'
+  , stylus = require('stylus')
+  , nib = require('nib');
+
+
+// Configure
+
+app.configure('development', function(){
+  var stylusMiddleware = stylus.middleware({
+    src: __dirname + '/stylus/', // .styl files are located in `/stylus`, must match /public folder structure
+    dest: __dirname + '/public/', // .styl resources are compiled `/stylesheets/*.css`
+    debug: true,
+    compile: function(str, path) { // optional, but recommended
+      return stylus(str)
+        .set('filename', path)
+        .set('warn', true)
+        .set('compress', true)
+        .use(nib());
+      }
+  });
+  app.use(stylusMiddleware);  
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+app.configure(function(){
+  app.set('views', __dirname + '/views/');
+  app.set('view engine', viewEngine);
+  app.use(express.logger(':method :url :status'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express['static'](__dirname + '/public'));
+  app.dynamicHelpers({
+    isDevMode: function (req, res) {
+      return (process.env.NODE_ENV || 'development') === 'development';
+    }
+  });
+});
+
+
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(8080);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+
+
+
