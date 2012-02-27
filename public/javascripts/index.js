de338ec7631ae5d24065a96e5959cd943a606a2d
Juha Rantanen <juha.rantanen@aalto.fi>
2012-02-27 17:11:51 +0200

    Custom css added.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,14 @@
-$(function(){
+$(function() {
+
+  $.mobile.showPageLoadingMsg();
+
+  $('body').delegate('.course-button', 'click', function() { 
+
+    $.mobile.changePage('#course-page');
+
+    console.log('Debugging...');
+
+  });
 
   mFeedback.randomizeLogo();
 
