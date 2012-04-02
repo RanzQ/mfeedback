eedda5819012392e3239e0df112ef5b85672dec5
Juha Rantanen <juha.rantanen@aalto.fi>
2012-04-02 12:38:16 +0300

    jQuery Mobile updated.

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -2,29 +2,15 @@ $(function() {
 
   mFeedback.randomizeLogo();
 
-  // This should enable swipe navigation, doesn't seem to work though
-  // $('body').delegate('.ui-page','swipeleft swiperight',function(event){
-  // 	if (event.type == "swiperight") {
-  // 		var prev = $("#prevlink",$.mobile.activePage);
-  // 		if (prev.length) {
-  // 			var prevurl = $(prev).attr("href");
-  // 			console.log(prevurl);
-  // 			$.mobile.changePage(prevurl);
-  // 		}
-  // 	}
-  // 	if (event.type == "swipeleft") {
-  // 		var next = $("#nextlink",$.mobile.activePage);
-  // 		if (next.length) {
-  // 			var nexturl = $(next).attr("href");
-  // 			console.log(nexturl);
-  // 			$.mobile.changePage(nexturl);
-  // 		}
-  // 	}
-  // 	event.preventDefault();
-  // });
-
   $('#search-form').submit(function(e) {
     if($('#query').val() === '') return false;
   });
 
+});
+
+// jQuery Mobile configuration
+$(document).bind("mobileinit", function(){
+  $.extend( $.mobile, {
+    defaultPageTransition: 'slide'
+  });
 });
